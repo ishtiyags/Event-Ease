a3e0c3383de91c7721c0fb82f6a088313443b5c3
ishtiyags <165825704+ishtiyags@users.noreply.github.com>
2026-03-06 17:27:19 +0200

    Add files via upload

---
+++ Models/Snack.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SnackTrackMvc.Models
+{
+    public class Snack
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Range(1, 100)]
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
